49f973a2a18f4dcce318ff43b02b108c536d43c3
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-06 15:40:39 +0700

    menambahkan isi

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -8,17 +8,18 @@ int n;
 void input()
 {
     while (true)
-}
-    cout << "Masukkan panjang array : ";
-    cin >> n;
+    {
+        cout << "Masukkan panjang array : ";
+        cin >> n;
 
-    if (n <=20)
-    {
-        break;
-    }
-    else
-    {
-        cout << "ERROR : Maksimal input array adalah 20\n";
+        if (n <=20)
+        {
+             break;
+        }
+        else
+        {
+            cout << "ERROR : Maksimal input array adalah 20\n";
+        }
     }
 
     cout << endl;
@@ -27,3 +28,47 @@ void input()
     cout << "====================="
 
     for (int i = 0; i < n; i++) //i++ /i = i+1
+    {
+        cout << "Masukkan Nilai ke - " << i + 1 << " : ";
+        cin >> a[i];
+    }
+}
+
+void display()
+{
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Elemen Array telah terurut" << endl;
+    cout << "==========================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke - " << i << "  : " << a[i] << endl;
+    
+    }
+}
+
+void BubbleSort()
+{
+    cout << "\n===============" << endl;
+    cout << "Hasil setiap Pass" << endl;
+    cout << "\n===============" << endl;
+    for (int pass = 1; pass < n; pass++)
+    {
+        for (int j = 0; j <= n -1 - pass; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+
+            }
+        }
+        cout << "Pass ke - " << pass << ":\t";
+        for (int k = 0; k < n; k ++)
+        {
+            cout << a[k] << "\t";
+        }
+    }
+}
