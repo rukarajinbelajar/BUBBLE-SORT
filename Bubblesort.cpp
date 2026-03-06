125693bc3d376c6854df3ab07d4de3f3b9adb167
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-06 15:47:51 +0700

    menambahkan endl di baris ke 26,27,28

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -23,9 +23,9 @@ void input()
     }
 
     cout << endl;
-    cout << "====================="
-    cout << "Masukkan elemen Array"
-    cout << "====================="
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen Array" << endl;
+    cout << "=====================" << endl;
 
     for (int i = 0; i < n; i++) //i++ /i = i+1
     {
