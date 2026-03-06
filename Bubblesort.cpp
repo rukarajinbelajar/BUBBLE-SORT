a67231a4aadb2876e247221dbac4f59a22b8bc62
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-06 15:24:43 +0700

    menghitung i++

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int a[[20];
+int a[20];
 int n;
 
 void input()
@@ -20,3 +20,10 @@ void input()
     {
         cout << "ERROR : Maksimal input array adalah 20\n";
     }
+
+    cout << endl;
+    cout << "====================="
+    cout << "Masukkan elemen Array"
+    cout << "====================="
+
+    for (int i = 0; i < n; i++) //i++ /i = i+1
